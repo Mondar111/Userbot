0bbb6cc6827eb41adc0f5af96756eee7e2e507a9
mrismanaziz <mrismanaziz@gmail.com>
2021-11-12 01:30:40 +0000

    Man-Userbot: auto fixes
    Signed-off-by: mrismanaziz <mrismanaziz@gmail.com>

--- userbot/modules/salam.py
+++ userbot/modules/salam.py
@@ -1,7 +1,7 @@
 from time import sleep
 
 from userbot import CMD_HANDLER as cmd
-from userbot import CMD_HELP, bot, user,
+from userbot import CMD_HELP, bot, user
 from userbot.events import man_cmd
 
 OWNER = user.first_name
