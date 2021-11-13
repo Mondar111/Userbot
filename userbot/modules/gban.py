4f74af7b8fbfe991e757f15356afd88f969977df
mrismanaziz <mrismanaziz@gmail.com>
2021-11-13 10:02:16 +0000

    Man-Userbot: auto fixes
    Signed-off-by: mrismanaziz <mrismanaziz@gmail.com>

--- userbot/modules/gban.py
+++ userbot/modules/gban.py
@@ -2,6 +2,7 @@
 # Lord Userbot
 
 from telethon import events
+
 from userbot import ALIVE_NAME, DEVS, bot
 from userbot.events import man_cmd, register
 from userbot.modules.sql_helper.gmute_sql import is_gmuted
