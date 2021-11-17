05b7fa5d989b223c75dfc8498cd40016234c1af3
Muhamad Risman Aziz <62795826+mrismanaziz@users.noreply.github.com>
2021-11-17 19:57:39 +0700

    Update __init__.py

--- userbot/modules/__init__.py
+++ userbot/modules/__init__.py
@@ -4,21 +4,5 @@
 # you may not use this file except in compliance with the License.
 #
 """ Init file which loads all of the modules """
-from userbot import LOGS
-
-
-def __list_all_modules():
-    import glob
-    from os.path import basename, dirname, isfile
-
-    mod_paths = glob.glob(dirname(__file__) + "/*.py")
-    return [
-        basename(f)[:-3]
-        for f in mod_paths
-        if isfile(f) and f.endswith(".py") and not f.endswith("__init__.py")
-    ]
-
-
-ALL_MODULES = sorted(__list_all_modules())
-LOGS.info("Modules To Load : %s", str(ALL_MODULES))
-__all__ = ALL_MODULES + ["ALL_MODULES"]
+from userbot import *
+from userbot.utils import *
