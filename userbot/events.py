b2d125156f4c8eadbd0679ed32cbb325568e4076
Mondar111 <86989223+Mondar111@users.noreply.github.com>
2021-11-25 05:06:54 +0700

    Update events.py

--- userbot/events.py
+++ userbot/events.py
@@ -182,7 +182,7 @@ def register(**args):
                 if not disable_errors:
                     date = strftime("%Y-%m-%d %H:%M:%S", gmtime())
 
-                    text = "**✘ MAN-USERBOT ERROR REPORT ✘**\n\n"
+                    text = "**🥷 MON-USERBOT ERROR REPORT 🥷**\n\n"
                     link = "[Group Support](https://t.me/SharingUserbot)"
                     text += "Jika mau, Anda bisa melaporkan error ini, "
                     text += f"Cukup forward saja pesan ini ke {link}.\n\n"
@@ -192,7 +192,7 @@ def register(**args):
                     ftext += "\nkami hanya mencatat fakta error dan tanggal,"
                     ftext += "\nkami menghormati privasi Anda."
                     ftext += "\nJika mau, Anda bisa melaporkan error ini,"
-                    ftext += "\ncukup forward saja pesan ini ke @SharingUserbot"
+                    ftext += "\ncukup forward saja pesan ini ke @mutualansesuka"
                     ftext += "\n================================\n\n"
                     ftext += "--------BEGIN USERBOT TRACEBACK LOG--------\n"
                     ftext += "\nTanggal : " + date
